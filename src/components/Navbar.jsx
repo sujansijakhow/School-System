93a496109bdf6bbdf785a188da405a2ca6a60f6b
sujansijakhow <sujansijakhow@gmail.com>
2025-03-30 21:29:13 +0545

    Navbar feature added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#B0B9D0] p-4 text-black flex h-[50] w-full'>
+        <div>
+            <input type="text" />
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
