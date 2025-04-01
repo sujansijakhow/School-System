321ea0ad374f1c79a96f7bd1b1e162d9be4bbae5
sujansijakhow <sujansijakhow@gmail.com>
2025-04-01 20:25:40 +0545

    input implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-[#B0B9D0] p-4 text-black flex h-[50] w-full'>
+      dfdf
         <div>
-            <input type="text" />
+            {/* <input type="text" /> */}
+            navbar
 
         </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
