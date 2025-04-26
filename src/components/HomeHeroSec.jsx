24f7196d3c3af5bd102dad562c94c938a67ef60d
sujansijakhow <sujansijakhow@gmail.com>
2025-04-26 10:36:05 +0545

    Updated

--- src/components/HomeHeroSec.jsx
+++ src/components/HomeHeroSec.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import EarningsChart from './EarningChart'
+import EventCalendar from './EventCalendar'
 
 const HomeHeroSec = () => {
   return (
-    <div className='flex flex-col gap-4'>
-        <div className='w-[] ml-4'>
-            <div className='flex justify-between  '>
-                <div className='w-60 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
+    <div className='flex flex-col gap-4 p-4 w-[]'>
+        
+            <div className='flex gap-34'>
+                <div className='w-50 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
                 <p className>student</p>
                 </div>
-                <div className='w-60 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
-                <p className>student</p>
+                <div className='w-50 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
+                <p className>Teacher</p>
                 </div>
-                <div className='w-60 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
-                <p className>student</p>
+                <div className='w-50 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
+                <p className>Total Employee</p>
                 </div>
-                <div className='w-60 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
-                <p className>student</p>
+                <div className='w-50 h-23 rounded-md bg-[#D9D9D9] flex items-center justify-center'>
+                <p className>Total Earning</p>
                 </div>
             </div>
-        </div>
-        <div className='flex'>
+        <div className='flex gap-4'>
             <EarningsChart />
+            <EventCalendar />
             <div>
                     
             </div>
