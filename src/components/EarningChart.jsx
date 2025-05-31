a45f1b8c2dd7785ae26703472dbfe5b840e93118
sujansijakhow <sujansijakhow@gmail.com>
2025-05-31 08:06:42 +0545

    updated

--- src/components/EarningChart.jsx
+++ src/components/EarningChart.jsx
@@ -22,7 +22,7 @@ const EarningsChart = () => {
   return (
     <div className="bg-[#D9D9D9] rounded-lg p-4 w-[500px] max-w-4xl mx-auto">
       <h2 className="text-lg font-semibold mb-4">Total Earning</h2>
-      <ResponsiveContainer width="100%" height={260}>
+      <ResponsiveContainer width="100%" height={200}>
         <BarChart data={data} barGap={0}>
           <XAxis dataKey="month" />
           <YAxis tickFormatter={(value) => `${value / 1000}k`} />
