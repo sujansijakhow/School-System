0bed758ad6a9af1724910eebd79bfe12e3adae89
sujansijakhow <sujansijakhow@gmail.com>
2025-06-11 12:11:00 +0545

    Component updated

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InputBox = ({ label, placeholder, type = "text", name, value, onChange }) => {
+const   InputBox = ({ label, placeholder, type = "text", name, value, onChange }) => {
   return (
     <div className="flex flex-col w-[15rem]">
       <label htmlFor={name} className="text-sm font-medium mb-1 text-black">
@@ -13,7 +13,7 @@ const InputBox = ({ label, placeholder, type = "text", name, value, onChange }) 
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="px-3 py-2 text-slate-900 bg-white outline-none rounded 
+        className="px-2 py-2 text-slate-900 bg-white outline-none rounded 
                    shadow-[4px_4px_6px_rgba(0,0,0,0.1)] focus:outline-none 
                    focus:ring-2 focus:ring-blue-400"
       />
