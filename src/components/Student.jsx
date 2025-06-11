c1fed141e9eb6662c7592fe39156877391e9ddce
sujansijakhow <sujansijakhow@gmail.com>
2025-06-11 12:11:27 +0545

    Feature completed

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -12,14 +12,14 @@ const Student = () => {
           <Sidebar />
           <div className='flex flex-col gap-3'>
             <Navbar />
-            <div className='h-full w-full flex flex-col gap-6 rounded-md p-8 bg-[#D9D9D9]'>
+            <div className='h-full w-full flex flex-col gap-4 rounded-md px-8 py-4 bg-[#D9D9D9]'>
               <div className='font-bold flex flex-col gap-1'>
                 <h1>Add new Student</h1>
                 <h1>Student Information</h1>
               </div>
-              <div className='flex flex-col gap-10'>
+              <div className='flex flex-col items-center gap-4'>
                 {/* first */}
-                <div className='flex items-center justify-between'>
+                <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4 lg:gap-16'>
                   <InputBox label={'First Name'} />
                   <InputBox label={'Last Name'} />
                   <SelectBox className="shadow-[4px_4px_6px_rgba(0,0,0,0.1)]"
@@ -50,23 +50,49 @@ const Student = () => {
                 </div>
 
                 {/* Second */}
-                <div className='flex items-center justify-between'>
-                  <InputBox label={'Gender'} />
-                  <InputBox label={'Date of Birth'} />
-                  <InputBox label={"ID No"} />
-                  <InputBox label={"Subject"} />
+                <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4 lg:gap-16">
+                  <InputBox label="Gender" />
+                  <InputBox label="Date of Birth" />
+                  <InputBox label="Roll" />
+                  <InputBox label="Admission No" />
                 </div>
+
 
                 {/* Third */}
-                <div className='flex items-center justify-between'>
+                <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4 lg:gap-16'>
                   <InputBox label={'Religion'} />
                   <InputBox label={'E-mail'} />
-                  <InputBox label={"Phone Number"} />
-                  <InputBox label={"Address"} />
-                  
+                  <div>
+                    <span className='text-sm font-medium mb-1 text-black'>
+                      Upload a student photo
+                    </span>
+                    <div>
+                      <button className='cursor-pointer border-1 border-amber-900 text-sm rounded-md px-3 py-2 shadow-[4px_4px_6px_rgba(0,0,0,0.1)] bg-white hover:bg-amber-50'>Choose a file</button>
+                    </div>
+                  </div>
+
 
                 </div>
-                
+
+                <div className='flex flex-col gap-4'>
+                  <h1 className='font-bold'>Parent Information</h1>
+                  <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4 lg:gap-16'>
+                    <InputBox label={"Father Name"} />
+                    <InputBox label={"Mother Name"} />
+                    <InputBox label={"Father Occupation"} />
+                    <InputBox label={"Father Occupation"} />
+                  </div>
+                  <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-8 lg:grid-cols-4 lg:gap-16'>
+                    <InputBox label={"Phone Number"} />
+                    <InputBox label={"Nationality"} />
+                    <InputBox label={"Present Address"} />
+                    <InputBox label={"Permanent Address"} />
+                  </div>
+                  <div className='flex gap-3 cursor-pointer'>
+                    <button className='bg-green-800 hover:bg-green-900 text-white p-2 rounded-sm w-22'>Save</button>
+                    <button className='bg-red-800 hover:bg-red-900 text-white p-2 rounded-sm w-22'>Reset</button>
+                  </div>
+                </div>
               </div>
             </div>
 
