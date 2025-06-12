07af522dde9af0d3272104058a1c3e943076ed7d
sujansijakhow <sujansijakhow@gmail.com>
2025-06-12 14:42:52 +0545

    Props updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Bell, Mail, Search, User } from 'lucide-react';
 import React, { useState } from 'react'
 
-const Navbar = () => {
+const Navbar = ({name}) => {
 
   const [isFocused, setIsFocused] = useState(false);
 
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className='flex w-[500px] justify-center md:gap-4 md:w-[1215px]'>
       <div className='bg-[#D9D9D9] text-black flex w-full h-[60px] md:w-full md:h-[80px] rounded-2xl flex-row items-center justify-between p-4'>
         <div className='flex items-center gap-5 md:gap-20'>
-          <div className='text-black text-[16px] md:text-2xl font-bold'>Dasboard</div>
+          <div className='text-black text-[16px] md:text-2xl font-bold'>{name}</div>
           <div
             className={`bg-[#B0B9D0] md:w-96 h-11 flex flex-row gap-5 items-center p-4 rounded-md transition-all 
         ${isFocused ? "border-1 border-slate-800" : "border border-transparent"}`}
