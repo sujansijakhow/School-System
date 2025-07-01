e0d574b851a58bbf40d7c774f122539424787683
sujansijakhow <sujansijakhow@gmail.com>
2025-07-01 20:06:29 +0545

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = ({name}) => {
     <div className='flex w-[500px] justify-center md:gap-4 md:w-[1215px]'>
       <div className='bg-[#D9D9D9] text-black flex w-full h-[60px] md:w-full md:h-[80px] rounded-2xl flex-row items-center justify-between p-4'>
         <div className='flex items-center gap-5 md:gap-20'>
+          {/* Props Implemented */}
           <div className='text-black text-[16px] md:text-2xl font-bold'>{name}</div>
           <div
             className={`bg-[#B0B9D0] md:w-96 h-11 flex flex-row gap-5 items-center p-4 rounded-md transition-all 
